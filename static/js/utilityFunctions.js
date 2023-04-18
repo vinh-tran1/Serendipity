49387d3263e76305becb60b3c446860c9f19476c
VinhTran <vinhtrann101@gmail.com>
2023-04-18 13:43:06 -0400

    some edits

--- static/js/utilityFunctions.js
+++ static/js/utilityFunctions.js
@@ -40,23 +40,19 @@ export function handAnyDetection(frame) {
     return 0;
 }
 
-// Return Grid Position
+// Returns if user is standing at envelope 1, 2, or 3
 export function getGridPosition(frame) {
-    // I don't think I need to normalize joint positions, I need absolute position?
-    // normalize by subtracting the pelvis joint coordinates
-    // var pelvis_x = frame.people[0].joints[0].position.x;
-    // var pelvis_y = frame.people[0].joints[0].position.y;
-    // var pelvis_z = frame.people[0].joints[0].position.z;
-    // var chest_x = (frame.people[0].joints[2].position.x - pelvis_x) * -1;
-    // var chest_y = (frame.people[0].joints[2].position.y - pelvis_y) * -1;
-    // var chest_z = (frame.people[0].joints[2].position.z - pelvis_z) * -1;
-
+    // Using absolute positioning instead of relative to pelvis
+    var leftBound = 0; var first_x = 100; var second_x = 400;
     var chest_x = frame.people[0].joints[2].position.x * -1;
-    var chest_y = frame.people[0].joints[2].position.y * -1;
-    var chest_z = frame.people[0].joints[2].position.z * -1;
+    var gridPostion = 1;
 
-    //store x,y,z coordinates of chest in array
-    const getGridPosition = [chest_x, chest_y, chest_z];
+    if (chest_x > leftBound && chest_x <= first_x) //first envelope
+        gridPostion = 1;
+    else if (chest_x > first_x && chest_x <= second_x) //second envelope
+        gridPostion = 2;
+    else //third envelope
+        gridPostion = 3;
 
-    return getGridPosition;
+    return gridPostion;
 }
