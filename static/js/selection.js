a9658549f0664a5228ba6f23fd201b73ebfcf6e3
VinhTran <vinhtrann101@gmail.com>
2023-04-19 12:37:56 -0400

    updates

--- static/js/selection.js
+++ static/js/selection.js
@@ -45,14 +45,7 @@ var frames = {
 // If user is in the same position for x time, then go to message.html
 export function positionProcess(frame) {
 
-    // Check whether they in message 1, message 2, or message 3 range (x-axis)
-    // Keep a counter of how long they stand in each grid position (1, 2, 3)
-    // Once a grid area reaches counter for 30 frames on it, then redirect to message!
-    // IDEA: counter array: [no one there, message 1, message 2, message 3]
-    //          counters[getGridPosition(frame)]++
-    //          if (counters[getGridPosition(frame)] > 30 && getGridPosition(frame) != 0) { // open message! }
-    //          else { // no one there, there to landing}
-    //var position = getGridPosition(frame);
+    // Check whether they're in message 1, message 2, or message 3 range (x-axis)
 
     var position = getGridPosition(frame);
     counter[position]++;
